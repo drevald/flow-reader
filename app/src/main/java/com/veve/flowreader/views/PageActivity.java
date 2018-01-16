4b116cb03d41ee86bc38bb794799ea355c4271b8
oudun <oudun@mail.ru>
2018-01-16 14:33:42 +0300

    Fake Page generated

--- app/src/main/java/com/veve/flowreader/views/PageActivity.java
+++ app/src/main/java/com/veve/flowreader/views/PageActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,11 +18,16 @@ import android.widget.ImageView;
 
 import com.veve.flowreader.Constants;
 import com.veve.flowreader.R;
+import com.veve.flowreader.model.Book;
+import com.veve.flowreader.model.BookPage;
+import com.veve.flowreader.model.BooksCollection;
+import com.veve.flowreader.model.PageGlyph;
 
 public class PageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -45,34 +51,30 @@ public class PageActivity extends AppCompatActivity {
             public void surfaceCreated(SurfaceHolder holder) {
                 Log.i("Tag?", "Surface created");
                 Canvas canvas = holder.lockCanvas();
-                if(canvas==null)
-                    Log.i("SurfaceHolder.Callback","canvas is null");
-                Log.i("SurfaceHolder.Callback","canvas width is " + canvas.getWidth()+ " canvas height is " + canvas.getHeight());
-                Paint paint = new Paint();
-                paint.setColor(Constants.GREEN);
-                canvas.drawColor(Color.CYAN);
-                canvas.drawRect(300, 200, 200, 400, paint);
-                holder.unlockCanvasAndPost(canvas);
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                Log.i("Tag?", "Surface changed");
-                Canvas canvas = holder.lockCanvas();
                 if(canvas==null) {
                     Log.i("SurfaceHolder.Callback", "canvas is null");
                 } else {
                     Log.i("SurfaceHolder.Callback", "canvas width is " + canvas.getWidth() + " canvas height is " + canvas.getHeight());
-//                    Paint paint = new Paint();
-//                    paint.setColor(Color.WHITE);
-//                    canvas.drawColor(Color.RED);
-//                    canvas.drawRect(300, 200, 200, 400, paint);
+
+                    Book book = BooksCollection.getInstance().getBooks().get(1);
+                    BookPage bookPage = book.getPage(book.getCurrentPageNumber());
+                    PageGlyph pageGlyph;
+                    Point startPoint = new Point(0, 0);
+                    while((pageGlyph = bookPage.getNextGlyph()) != null) {
+                        startPoint = pageGlyph.draw(startPoint, canvas, 1);
+                    }
+                    holder.unlockCanvasAndPost(canvas);
                 }
             }
 
             @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                Log.i("SurfaceHolder.Callback", "Surface changed");
+            }
+
+            @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
-                Log.i("Tag?", "Surface destroyed");
+                Log.i("SurfaceHolder.Callback", "Surface destroyed");
             }
         });
 
