2cfc601657e8440e93607cd487f9dbe3240962e5
oudun <8adagris>
2018-07-24 12:02:18 +0300

    Logs and tests added

--- app/src/test/java/com/veve/flowreader/ExampleUnitTest.java
+++ app/src/test/java/com/veve/flowreader/ExampleUnitTest.java
@@ -1,5 +1,17 @@
 package com.veve.flowreader;
 
+import android.graphics.Canvas;
+import android.graphics.Point;
+
+import com.veve.flowreader.dao.BookRecord;
+import com.veve.flowreader.model.BookSource;
+import com.veve.flowreader.model.DevicePageContext;
+import com.veve.flowreader.model.PageRenderer;
+import com.veve.flowreader.model.PageRendererFactory;
+import com.veve.flowreader.model.impl.DevicePageContextImpl;
+import com.veve.flowreader.model.impl.PageRendererImpl;
+import com.veve.flowreader.model.impl.djvu.DjvuBookSource;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +22,23 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+
+    @Test
+    public void testDjvu() throws Exception {
+        BookSource bookSource = new DjvuBookSource("src/main/res/raw/djvu_sample.djvu");
+        PageRenderer renderer = new PageRendererImpl(bookSource);
+        DevicePageContext context = new DevicePageContextImpl();
+        renderer.renderPage(context, 1);
+    }
+
 }
