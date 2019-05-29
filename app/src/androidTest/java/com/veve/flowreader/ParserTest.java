45ea1c8e4c26b49395c1c44f778002fd9cfc0dda
Wood Pecker <kroshka.dyatel@gmail.com>
2019-05-30 00:52:22 +0300

    PageSegmenter text set to parse two glyphs. Fails anyway

--- app/src/androidTest/java/com/veve/flowreader/ParserTest.java
+++ app/src/androidTest/java/com/veve/flowreader/ParserTest.java
@@ -3,12 +3,16 @@ package com.veve.flowreader;
 import android.graphics.Bitmap;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.veve.flowreader.model.PageGlyph;
+import com.veve.flowreader.model.impl.PageSegmenter;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.nio.Buffer;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class ParserTest {
@@ -25,10 +29,10 @@ public class ParserTest {
                 int index = 4 * (i + (j * width));
                 byte color = (byte) 255;
                 byte opacity = (byte) 255;
-                if (i > 3 && i < 6 && j > 3 && j < 6) {
+                if (i > 3 && i < 6 && ((j > 3 && j < 6) || (j > 9 && j <12))) {
                     color = (byte)0;
                 }
-                buffer.put(index + 0, (byte) color);
+                buffer.put(index, (byte) color);
                 buffer.put(index + 1, (byte) color);
                 buffer.put(index + 2, (byte) color);
                 buffer.put(index + 3, (byte) opacity);
@@ -36,6 +40,10 @@ public class ParserTest {
         }
         Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         bitmap.copyPixelsFromBuffer(buffer);
+        PageSegmenter pageSegmenter = new PageSegmenter();
+        List<PageGlyph> pageGlyphs = pageSegmenter.getGlyphs(bitmap);
+        assertEquals(pageGlyphs.size(), 2);
+
     }
 
 }
