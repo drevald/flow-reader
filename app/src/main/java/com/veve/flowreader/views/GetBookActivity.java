73d37f8bbb89625a975ecf699f03c0723e13e667
oudun <8adagris>
2019-08-21 18:51:51 +0300

    Make icon visible. Add intermediate GetBookActivity

---
+++ app/src/main/java/com/veve/flowreader/views/GetBookActivity.java
@@ -0,0 +1,15 @@
+package com.veve.flowreader.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.veve.flowreader.R;
+
+public class GetBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_book);
+    }
+}
