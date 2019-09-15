7981e97165734aa7fbceae8ac0067d96af1d41ef
oudun <oudun@mail.ru>
2019-09-15 11:56:20 +0300

    Flip pages by scrolling

--- app/src/main/java/com/veve/flowreader/Constants.java
+++ app/src/main/java/com/veve/flowreader/Constants.java
@@ -19,7 +19,7 @@ public interface Constants {
     String VIEW_TYPE = "VIEW_TYPE";
     int LIST_VIEW_TYPE = 0;
     int GRID_VIEW_TYPE = 1;
-    boolean DEBUG = false;
+    boolean DEBUG = true;
     int MAX_BITMAP_SIZE = 100 * 1024 * 1024; // 100 MB
     int IMAGE_VIEW_HEIGHT_LIMIT = 4000;
 
