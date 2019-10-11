8d895c81651c66fa57b4ff61d7abb87e19b0dbb1
oudun <8adagris>
2019-10-11 13:40:49 +0300

    Performance test refactored

--- app/src/androidTest/java/com/veve/flowreader/PerformanceTest.java
+++ app/src/androidTest/java/com/veve/flowreader/PerformanceTest.java
@@ -22,6 +22,7 @@ import org.junit.runner.RunWith;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.util.function.BiConsumer;
 
 import static org.junit.Assert.assertTrue;
 
@@ -64,38 +65,12 @@ public class PerformanceTest {
         CachedPageRendererImpl cachedPageRenderer = new CachedPageRendererImpl(booksCollection, bookRecord, djvuBookSource);
         DevicePageContext context = new DevicePageContextImpl(800);
 
-        start = System.currentTimeMillis();
-        cachedPageRenderer.renderOriginalPage(context, 0);
-        Log.d(getClass().getName(), "First opening original took " + (System.currentTimeMillis() - start) + " millisecods");
-        assertTrue(System.currentTimeMillis() - start < 1000);
-
-        start = System.currentTimeMillis();
-        cachedPageRenderer.renderOriginalPage(context, 0);
-        Log.d(getClass().getName(), "Second opening original took " + (System.currentTimeMillis() - start) + " millisecods");
-        assertTrue(System.currentTimeMillis() - start < 500);
-
-
-        start = System.currentTimeMillis();
-        cachedPageRenderer.renderPage(context, 0);
-        Log.d(getClass().getName(), "First opening page took " + (System.currentTimeMillis() - start) + " millisecods");
-        assertTrue(System.currentTimeMillis() - start < 3000);
-
-
-        start = System.currentTimeMillis();
-        cachedPageRenderer.renderPage(context, 0);
-        Log.d(getClass().getName(), "Second opening page took " + (System.currentTimeMillis() - start) + " millisecods");
-        assertTrue(System.currentTimeMillis() - start < 1000);
-
-        start = System.currentTimeMillis();
-        cachedPageRenderer.renderPage(context, 1);
-        Log.d(getClass().getName(), "First opening next page took " + (System.currentTimeMillis() - start) + " millisecods");
-        assertTrue(System.currentTimeMillis() - start < 3000);
-
-        start = System.currentTimeMillis();
-        cachedPageRenderer.renderPage(context, 0);
-        Log.d(getClass().getName(), "Third opening page took " + (System.currentTimeMillis() - start) + " millisecods");
-        assertTrue(System.currentTimeMillis() - start < 1000);
-
+        checkTiming(cachedPageRenderer::renderOriginalPage, context, 0, 2000, "First time opening original page");
+        checkTiming(cachedPageRenderer::renderOriginalPage, context, 0, 500, "Second time opening original page");
+        checkTiming(cachedPageRenderer::renderPage, context, 0, 3000, "First time opening page");
+        checkTiming(cachedPageRenderer::renderPage, context, 0, 1000, "Second time opening page");
+        checkTiming(cachedPageRenderer::renderPage, context, 1, 3000, "First time opening next page");
+        checkTiming(cachedPageRenderer::renderPage, context, 0, 1000, "Third time opening page");
 
     }
 
@@ -104,6 +79,12 @@ public class PerformanceTest {
        booksCollection.deleteBook(bookRecordId);
     }
 
-
+    private void checkTiming(BiConsumer<DevicePageContext, Integer> f, DevicePageContext context, int position, int limit, String message) {
+        long start = System.currentTimeMillis();
+        f.accept(context, position);
+        long end = System.currentTimeMillis();
+        Log.d(getClass().getName(), message + " took " + (System.currentTimeMillis() - start) + " millisecods");
+        assertTrue(message + " took " + (end - start ) + " milliseconds which is more than " + limit, end - start < limit);
+    }
 
 }
