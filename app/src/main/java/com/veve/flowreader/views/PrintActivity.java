c734dc71e9fe1c4b05d456a58e1714ad558ad332
oudun <8adagris>
2019-12-18 19:13:02 +0300

    Working on print preview

---
+++ app/src/main/java/com/veve/flowreader/views/PrintActivity.java
@@ -0,0 +1,205 @@
+package com.veve.flowreader.views;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.veve.flowreader.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.CancellationSignal;
+import android.os.ParcelFileDescriptor;
+import android.print.PageRange;
+import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintDocumentInfo;
+import android.print.PrintManager;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.graphics.Bitmap.Config.ARGB_4444;
+
+public class PrintActivity extends AppCompatActivity {
+
+    class MyPrintDocumentAdapter extends PrintDocumentAdapter {
+
+        public MyPrintDocumentAdapter(Activity activity) {
+            Log.v(getClass().getName(), "Construct");
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            Log.v(getClass().getName(), "on start");
+        }
+
+        @Override
+        public void onLayout(PrintAttributes oldAttributes,
+                             PrintAttributes newAttributes,
+                             CancellationSignal cancellationSignal,
+                             LayoutResultCallback callback,
+                             Bundle extras) {
+         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            Log.v(getClass().getName(), "onLayout");
+            PrintDocumentInfo info = new PrintDocumentInfo
+                    .Builder("print_output.pdf")
+                    .setContentType(PrintDocumentInfo.CONTENT_TYPE_DOCUMENT)
+                    .setPageCount(10)
+                    .build();
+                callback.onLayoutFinished(info, false);
+                Log.v(getClass().getName(), "onLayout 1");
+            }
+        }
+
+        @Override
+        public void onWrite(final PageRange[] pageRanges,
+                            final ParcelFileDescriptor destination,
+                            final CancellationSignal cancellationSignal,
+                            final WriteResultCallback callback) {
+            Log.v(getClass().getName(), "onWrite");
+
+            PdfDocument pdfDocument = new PdfDocument();
+
+//            PdfDocument.PageInfo.Builder pageOneBuilder = new PdfDocument.PageInfo.Builder(100, 100, 1);
+//            PdfDocument.Page page = pdfDocument.startPage(pageOneBuilder.create());
+//            Canvas canvas = page.getCanvas();
+//            Bitmap bitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), ARGB_4444);
+//            Paint paint = new Paint();
+//            paint.setStyle(Paint.Style.FILL);
+//            paint.setColor(Color.BLUE);
+//            canvas.drawText("HERE I AM", 100, 100, paint);
+//            canvas.drawRect(100, 100, 100, 100, paint);
+//
+//            View content = findViewById(R.id.box);
+//            content.draw(page.getCanvas());
+//
+//            pdfDocument.finishPage(page);
+//            try{
+//                File file = new File(getApplicationContext().getExternalFilesDir(null), "print.pdf");
+//                pdfDocument.writeTo(new FileOutputStream(file));
+//                pdfDocument.writeTo(new FileOutputStream(destination.getFileDescriptor()));
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//
+//            callback.onWriteFinished(new PageRange[]{PageRange.ALL_PAGES});
+
+            // Iterate over each page of the document,
+            // check if it's in the output range.
+            //for (int i = 0; i < totalPages; i++) {
+            for (int i = 0; i < 5; i++) {
+                // Check to see if this page is in the output range.
+                //if (containsPage(pageRanges, i)) {
+                    // If so, add it to writtenPagesArray. writtenPagesArray.size()
+                    // is used to compute the next output page index.
+                    //writtenPagesArray.append(writtenPagesArray.size(), i);
+                    //PdfDocument.Page page = pdfDocument.startPage(i);
+
+                    PdfDocument.PageInfo.Builder pageBuilder = new PdfDocument.PageInfo.Builder(100, 100, i);
+                    PdfDocument.Page page = pdfDocument.startPage(pageBuilder.create());
+
+                    // check for cancellation
+                    if (cancellationSignal.isCanceled()) {
+                        callback.onWriteCancelled();
+                        pdfDocument.close();
+                        pdfDocument = null;
+                        return;
+                    }
+
+                    // Draw page content for printing
+                    // drawPage(page);
+
+//                    View content = findViewById(R.id.box);
+//                    content.draw(page.getCanvas());
+                    //Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.ic_icon_flowbook);
+
+                    Bitmap bitmap = Bitmap.createBitmap(160, 160,  Bitmap.Config.ARGB_8888);
+                    Canvas canvas = new Canvas(bitmap);
+                    Paint paint = new Paint();
+                    paint.setColor(Color.GREEN);
+                    paint.setStyle(Paint.Style.FILL);
+                    canvas.drawRect(0, 0, 50, 50, paint);
+
+                    page.getCanvas().drawBitmap(bitmap, 0, 0, new Paint());
+
+                    // Rendering is complete, so page can be finalized.
+                    pdfDocument.finishPage(page);
+                //}
+            }
+//
+            // Write PDF document to file
+            try {
+                pdfDocument.writeTo(new FileOutputStream(
+                        destination.getFileDescriptor()));
+            } catch (IOException e) {
+                callback.onWriteFailed(e.toString());
+                return;
+            } finally {
+                pdfDocument.close();
+                pdfDocument = null;
+            }
+            //PageRange[] writtenPages = computeWrittenPages();
+            // Signal the print framework the document is complete
+            //callback.onWriteFinished(writtenPages);
+            callback.onWriteFinished(new PageRange[]{PageRange.ALL_PAGES});
+
+//
+//    //...
+        }
+
+    }
+
+    private void doPrint() {
+        // Get a PrintManager instance
+        PrintManager printManager = (PrintManager) getActivity()
+                .getSystemService(Context.PRINT_SERVICE);
+
+        // Set job name, which will be displayed in the print queue
+        String jobName = getActivity().getString(R.string.app_name) + " Document";
+
+        // Start a print job, passing in a PrintDocumentAdapter implementation
+        // to handle the generation of a print document
+        printManager.print(jobName, new MyPrintDocumentAdapter(getActivity()),
+                null); //
+    }
+
+    private Activity getActivity() {
+        return this;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_print);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                doPrint();
+
+            }
+        });
+    }
+
+}
