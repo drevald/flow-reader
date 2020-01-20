161ff8e5134e3f027219009ca41f6bfccaafaa57
oudun <oudun@mail.ru>
2020-01-20 22:44:48 +0300

    Test added

--- app/src/androidTest/java/com/veve/flowreader/BitmapMemoryTest.java
+++ app/src/androidTest/java/com/veve/flowreader/BitmapMemoryTest.java
@@ -22,8 +22,8 @@ public class BitmapMemoryTest {
         Bitmap.Config bitmapConfig = Bitmap.Config.ARGB_8888;
         int width = 1000;
         Bitmap bm;
-        for (int height = 1000; height < 100000; height += 1000) {
-            Log.d(getClass().getName(), String.format("Building bitimaз %d x %d", width, height));
+        for (int height = 1000; height < 33000; height += 1000) {
+            Log.d(getClass().getName(), String.format("Building bitimap %d x %d", width, height));
             bm = Bitmap.createBitmap(width, height, bitmapConfig);
             bm = null;
             System.gc();
