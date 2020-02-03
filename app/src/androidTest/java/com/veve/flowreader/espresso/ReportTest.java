a361f76baf9eae0eed497088485c0257e546fe87
oudun <oudun@mail.ru>
2020-02-04 00:25:48 +0300

    'Touch' View Action added

--- app/src/androidTest/java/com/veve/flowreader/espresso/ReportTest.java
+++ app/src/androidTest/java/com/veve/flowreader/espresso/ReportTest.java
@@ -1,8 +1,13 @@
 package com.veve.flowreader.espresso;
 
 import android.content.Intent;
+import android.view.MotionEvent;
+import android.view.View;
 
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.MotionEvents;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -14,6 +19,7 @@ import com.veve.flowreader.uitest.BookTest;
 import com.veve.flowreader.views.MainActivity;
 import com.veve.flowreader.views.PageActivity;
 
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -26,6 +32,7 @@ import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
@@ -50,13 +57,47 @@ public class ReportTest  extends BookTest {
 
     @Test
     public void testSendReport() {
+        onIdle();
+        onView(ViewMatchers.withId(R.id.page)).perform(touchCenter());
         openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
         //Espresso.onView(ViewMatchers.withId(R.id.page_unreadable)).perform(click());
         onView(ViewMatchers.withText("Page unreadable")).perform(click());
         onIdle();
         onView(withId(R.id.send)).perform(click());
         onIdle();
         assert(true);
+    }
+
+    public static ViewAction touchCenter() {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isDisplayed();
+            }
+
+            @Override
+            public String getDescription() {
+                return "Touches center of view";
+            }
+
+            @Override
+            public void perform(UiController uiController, final View view) {
+                // Get view absolute position
+                int[] location = new int[2];
+                view.getLocationOnScreen(location);
+
+                // Offset coordinates by view position
+                float[] coordinates = new float[] {(view.getLeft() + view.getRight())/2,
+                        (view.getTop() + view.getBottom())/2};
+                float[] precision = new float[] { 1f, 1f };
+
+                // Send down event, pause, and send up
+                MotionEvent down = MotionEvents.sendDown(uiController, coordinates, precision).down;
+                uiController.loopMainThreadForAtLeast(200);
+                MotionEvents.sendUp(uiController, down, coordinates);
+            }
+        };
     }
 
 
