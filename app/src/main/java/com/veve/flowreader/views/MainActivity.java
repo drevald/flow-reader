37d6175f1307f1fd30daaddcbefba99356d844f3
Sergey Mikhno <sergey.mikhno@gmail.com>
2020-02-17 19:48:29 +0100

    added null check for the case if nothing is selected

--- app/src/main/java/com/veve/flowreader/views/MainActivity.java
+++ app/src/main/java/com/veve/flowreader/views/MainActivity.java
@@ -158,7 +158,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == FILE_OPEN_REQUEST) {
+        if (data != null && requestCode == FILE_OPEN_REQUEST) {
             Uri uri = data.getData();
 
             try {
