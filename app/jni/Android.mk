3a0567055fd393e3224aa8bdd12b4d6433834dac
Denis Dreval <dreval@narod.ru>
2025-07-18 00:25:25 +0300

    native config adjusted

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -1,3 +1,6 @@
 LOCAL_SHORT_COMMANDS := true
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles)
+LOCAL_CFLAGS    += -fopenmp -static-openmp
+LOCAL_CPPFLAGS  += -fopenmp -static-openmp
+LOCAL_LDLIBS    += -static-openmp
