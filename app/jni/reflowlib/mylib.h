237285ac233d8ce037340d04443a4c38f008fcca
Sergey Mikhno <sergey.mikhno@galenica.com>
2025-07-18 17:30:06 +0200

    extern C

--- app/jni/reflowlib/mylib.h
+++ app/jni/reflowlib/mylib.h
@@ -1,9 +1,9 @@
 #ifndef FLOW_READER_MYLIB_H
 #define FLOW_READER_MYLIB_H
 
-#if __ANDROID_API__ < __ANDROID_API_L__
-double atof(const char *nptr);
-#endif
+// #if __ANDROID_API__ < __ANDROID_API_L__
+extern  "C" double atof(const char *nptr);
+// #endif
 
 #endif
 
