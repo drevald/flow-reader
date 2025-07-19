a2dbc4de642689ceb6c0cedef84dbcc1914b8e44
Sergey Mikhno <sergey.mikhno@galenica.com>
2025-07-19 10:40:01 +0200

    compile with 17

--- app/jni/reflowlib/mylib.cpp
+++ app/jni/reflowlib/mylib.cpp
@@ -1,11 +1,6 @@
 #include "mylib.h"
 #include "common.h"
 
-
 double atof(const char *nptr) {
     return (strtod(nptr, NULL));
 }
-
-
-
-
--- app/jni/reflowlib/mylib.h
+++ app/jni/reflowlib/mylib.h
@@ -1,10 +1,5 @@
 #ifndef FLOW_READER_MYLIB_H
 #define FLOW_READER_MYLIB_H
 
-// #if __ANDROID_API__ < __ANDROID_API_L__
 extern  "C" double atof(const char *nptr);
-// #endif
-
 #endif
-
-
